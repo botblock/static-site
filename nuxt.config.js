d6c1625fa2ad0b77f34b08e021e39e5b90f2aad9
MattIPv4 <me@mattcowley.co.uk>
2021-12-11 23:11:06 +0000

    Add some base meta tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,11 +8,45 @@ const jsonData = dir => fs.readdirSync(path.join(__dirname, 'data', 'data', dir)
 module.exports = {
     target: 'static',
     head: {
+        htmlAttrs: { lang: 'en' },
+        title: 'BotBlock',
         meta: [
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+
+            { name: 'mobile-web-app-capable', content: 'yes' },
+            { name: 'apple-mobile-web-app-capable', content: 'yes' },
+            { name: 'theme-color', content: '#29a3d0' },
+
+            { hid: 'title', name: 'title', content: 'BotBlock' },
+            { hid: 'description', name: 'description', content: 'BotBlock' },
+            { hid: 'keywords', name: 'keywords', content: 'BotBlock' },
+
+            { name: 'twitter:card', content: 'summary_large_image' },
+            { name: 'twitter:site', content: '@BlockBotOrg' },
+            { name: 'twitter:creator', content: '@MattIPv4' },
+            { hid: 'twitter:title', name: 'twitter:title', content: 'BotBlock' },
+            { hid: 'twitter:description', name: 'twitter:description', content: 'BotBlock' },
+            { name: 'twitter:image', content: '/header.png' },
+            { name: 'twitter:image:alt', content: 'BotBlock header image' },
+
+            { hid: 'og:title', prefix: 'og: http://ogp.me/ns#', property: 'og:title', content: 'BotBlock' },
+            { prefix: 'og: http://ogp.me/ns#', property: 'og:type', content: 'website' },
+            { prefix: 'og: http://ogp.me/ns#', property: 'og:locale', content: 'en_GB' },
+            { prefix: 'og: http://ogp.me/ns#', property: 'og:site_name', content: 'BotBlock' },
+            { hid: 'og:description', prefix: 'og: http://ogp.me/ns#', property: 'og:description', content: 'BotBlock' },
+            { hid: 'og:url', prefix: 'og: http://ogp.me/ns#', property: 'og:url', content: 'https://botblock.org' },
+            { prefix: 'og: http://ogp.me/ns#', property: 'og:image', content: '/header.png' },
+            { prefix: 'og: http://ogp.me/ns#', property: 'og:image:alt', content: 'BotBlock header image' },
+        ],
+        link: [
+            { rel: 'icon', type: 'image/png', href: '/icon.png' },
+            { rel: 'shortcut-icon', type: 'image/png', href: '/icon.png' },
+            { rel: 'apple-touch-icon', type: 'image/png', href: '/icon.png' },
+            { hid: 'canonical', rel: 'canonical', href: 'https://botblock.org' },
         ],
     },
+    loading: { color: '#29a3d0' },
     css: [ 'modern-normalize' ],
     buildModules: [ 'nuxt-resolve-url-loader' ],
     generate: {
